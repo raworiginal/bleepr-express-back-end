c6783868711a2267a1bce86eb08cde7d88a37214
raworiginal <randall.a.wolfe@gmail.com>
2025-08-21 13:24:52 -0700

    created bleepr model

---
+++ models/bleepr.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+// a bleepr is a user
+const bleeprSchema = mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+	},
+	hashedPassword: {
+		type: String,
+		required: true,
+	},
+	friends: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Bleepr",
+		},
+	],
+	top8: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Bleepr",
+		},
+	],
+	favoritedBleeps: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Bleep",
+		},
+	],
+	isOnline: {
+		type: Boolean,
+	},
+	offlineMsg: {
+		type: String,
+	},
+	aboutMe: {
+		type: aboutMeSchema,
+	},
+});
+userSchema.set("toJSON", {
+	transform: (document, returnedObject) => {
+		delete returnedObject.hashedPassword;
+	},
+});
+const Bleepr = mongoose.model("Bleepr", userSchema);
+
+module.exports = Bleepr;
