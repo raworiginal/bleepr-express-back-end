836224b42b88197ca639530e7e0cf152c5aef71d
raworiginal <randall.a.wolfe@gmail.com>
2025-08-22 10:42:48 -0700

    Completed the bleep show route

--- controllers/bleeps.js
+++ controllers/bleeps.js
@@ -7,33 +7,42 @@ const router = express.Router();
 
 // POST /bleeps
 router.post("/", verifyToken, async (req, res) => {
-    try {
-        req.body.author = req.bleepr._id
-        const bleep = await Bleep.create(req.body);
-        bleep._doc.author = req.bleepr;
-        res.status(201).json(bleep);
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
-})
+	try {
+		req.body.author = req.bleepr._id;
+		const bleep = await Bleep.create(req.body);
+		bleep._doc.author = req.bleepr;
+		res.status(201).json(bleep);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+});
 
 // =========================== READ ============================ //
 
 //GET /bleeps
 router.get("/", verifyToken, async (req, res) => {
-    try {
-        const bleeps = await Bleep.find({})
-            .populate("author")
-            .sort({ createdAt: "desc" });
-        res.status(200).json(bleeps)
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
+	try {
+		const bleeps = await Bleep.find({})
+			.populate("author")
+			.sort({ createdAt: "desc" });
+		res.status(200).json(bleeps);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+});
+
+// GET /bleeps/:bleepId
+router.get("/:bleepId", verifyToken, async (req, res) => {
+	try {
+		const bleep = await Bleep.findById(req.params.bleepId).populate("author");
+		res.status(200).json(bleep);
+	} catch (error) {
+		res.status(500);
+	}
 });
 
 //============================UPDATE =========================== //
 
 // ========================== DELETE =========================== //
-
 
 module.exports = router;
