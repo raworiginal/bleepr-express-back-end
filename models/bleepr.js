40c8b405b23fafb918cc755f4177f0bb7c16664e
raworiginal <randall.a.wolfe@gmail.com>
2025-08-22 08:46:21 -0700

    updated bleepr

--- models/bleepr.js
+++ models/bleepr.js
@@ -1,36 +1,41 @@
 const mongoose = require("mongoose");
 
 // a bleepr is a user
- const aboutMeSchema = mongoose.Schema({
+const aboutMeSchema = mongoose.Schema({
 	name: {
-		type: String
+		type: String,
 	},
 	age: {
-		type: String
+		type: String,
 	},
 	gender: {
-		type: String
+		type: String,
 	},
 	location: {
-		type: String
+		type: String,
 	},
 	bio: {
-		type: String
+		type: String,
 	},
 	openTo: {
 		type: String,
-		Enum: ["networking", "dating", "making friends", "mentoring"]
+		Enum: ["networking", "dating", "making friends", "mentoring"],
 	},
 	notOpenTo: {
 		type: String,
-		Enum: ["networking", "dating", "making friends", "mentoring"]
+		Enum: ["networking", "dating", "making friends", "mentoring"],
 	},
 	relationshipStatus: {
 		type: String,
-		Enum: ["single", "in a relationahip", "married", "it's complicated", "open"]
-	}
- })
-
+		Enum: [
+			"single",
+			"in a relationahip",
+			"married",
+			"it's complicated",
+			"open",
+		],
+	},
+});
 const bleeprSchema = mongoose.Schema({
 	username: {
 		type: String,
